90d9fe49a6518e5bfa65dd9e68a3ec287bdce064
marcstreeter <mstreeter@gmail.com>
2016-10-25 14:04:15 -0500

    Finished Lecture 58 added if statement and a exclamation mark and tasks ❗️

---
+++ DoIt 5/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt 5
+//
+//  Created by Marc Streeter on 10/25/16.
+//  Copyright © 2016 Developer Warriors. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+   var name = ""
+   var important = false
+}
--- DoIt 5/ViewController.swift
+++ DoIt 5/ViewController.swift
@@ -12,26 +12,48 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     @IBOutlet weak var tableView: UITableView!
 
+    var tasks : [Task] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-    tableView.dataSource = self
+    tasks = makeTasks()
+        
+        tableView.dataSource = self
     tableView.delegate = self
     
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
  
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-       cell.textLabel?.text = "Hello"
+       let task = tasks[indexPath.row]
+        if task.important {
+         cell.textLabel?.text = "❗️\(task.name)"
+        }else{
+         cell.textLabel?.text = task.name
+        }
       return cell
     }
-
+    func makeTasks() -> [Task] {
+       let task1 = Task()
+       task1.name = "Buy Clothes"
+       task1.important = true
+   
+        let task2 = Task()
+        task2.name = "Go to the Prix"
+        task2.important = false
+   
+        let task3 = Task()
+        task3.name = "Make a halloween party"
+        task3.important = true
+ 
+       return [task1,task2,task3]
+    }
 
 }
 
