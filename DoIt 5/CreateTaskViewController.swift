a4e092a760fe3e6cfb9973246517f05aee8f50cd
marcstreeter <mstreeter@gmail.com>
2016-10-25 20:25:58 -0500

    finished lecture 60 added addTapped and CreateTaskViewController.swift and gonna make an action for the button to do

---
+++ DoIt 5/CreateTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt 5
+//
+//  Created by Marc Streeter on 10/25/16.
+//  Copyright © 2016 Developer Warriors. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+
+    
+}
